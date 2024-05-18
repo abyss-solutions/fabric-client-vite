095ad7b98f9ca01c3716b046ccf6c87bafacc473
usman-abyss <u.ahmed@abysssolutions.com.au>
2024-05-18 21:57:36 +0500

    fix: local ssl

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,32 @@
 import fs from "fs";
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, `./localhost-key.pem`)),
-      cert: fs.readFileSync(path.resolve(__dirname, `./localhost.pem`)),
-    },
-    host: "0.0.0.0",
-  },
+export default defineConfig(({ mode }) => {
+  const isDev = mode === "development";
 
-  // path aliases
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+      https: isDev
+        ? {
+            key: fs.readFileSync(
+              path.resolve(__dirname, `./localhost-key.pem`)
+            ),
+            cert: fs.readFileSync(path.resolve(__dirname, `./localhost.pem`)),
+          }
+        : {},
+      host: "0.0.0.0",
     },
-  },
+
+    // path aliases
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
 });
