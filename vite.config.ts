2172f7ddc6726387e85385060917fed4ba9db3b0
usman-abyss <u.ahmed@abysssolutions.com.au>
2024-05-19 17:20:46 +0500

    refactor: remove unused

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import fs from "fs";
 import path from "path";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
